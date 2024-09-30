927851583f4ed0c2feaf4e208d1cd42324e1d013
matheusmiranda96 <matth.miranda@gmail.com>
2024-09-30 13:24:36 -0300

    handleSearch

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,14 +1,23 @@
 import { HiOutlineSearch } from 'react-icons/hi';
 import styles from './SearchBar.module.css'
 import { useState } from 'react';
+import fetchProducts from '../../../api/fetchProducts'
 
 function SearchBar() {
 
     const [searchValue, setSearchValue] = useState('');
 
+    const handleSearch = async (event) => {
+        event.preventDefault();
+        
+        const products = await fetchProducts(searchValue);
+        console.log(products);
+        setSearchValue('');
+    }
+
     return ( 
         <>
-            <form className={styles.search__bar}>
+            <form className={styles.search__bar} onSubmit={handleSearch}>
                 
                 <input type="search" 
                 value={searchValue}
