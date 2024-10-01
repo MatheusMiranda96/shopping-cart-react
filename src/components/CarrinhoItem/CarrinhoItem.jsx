0fb6d8c32fec224764b014080eea12aafce94f70
matheusmiranda96 <matth.miranda@gmail.com>
2024-10-01 12:42:46 -0300

    formatcurrency

--- src/components/CarrinhoItem/CarrinhoItem.jsx
+++ src/components/CarrinhoItem/CarrinhoItem.jsx
@@ -16,7 +16,7 @@ function CarrinhoItem({ data }) {
 
             <div className={styles.cart_item_content}>
                 <h3 className={styles.cart_item_title}>{ title }</h3>
-                <h3 className={styles.cart_item_price}>{price}</h3>
+                <h3 className={styles.cart_item_price}>{ formatCurrency(price, 'BRL') }</h3>
             
                 <button type='button' className={styles.btn_remover_item}>
                     <BsCartDashFill />
